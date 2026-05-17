cb49a4e3ba894521434d7d94040f9631ab893e22
mohamed_abdo <mmdabdo94@gmail.com>
2026-05-17 11:15:14 +0300

    * add image cart

--- lib/core/model/search_shopping.dart
+++ lib/core/model/search_shopping.dart
@@ -9,55 +9,57 @@ class SearchShopping extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Padding(
-      padding:  EdgeInsets.symmetric(horizontal: size.width*0.02,vertical: size.height*0.055),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SizedBox(
-            width: size.width * 0.8,
-            height: size.height * 0.05,
-            child: TextFormField(
-              decoration: InputDecoration(
-                hintText: 'What do you search for ?',
-                hintStyle: TextStyle(color: AppColor.primary, fontSize: 16),
-                focusColor: AppColor.white,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(28),
-                  borderSide: BorderSide(color: AppColor.primary, width: 2),
+    return SafeArea(
+      child: Padding(
+        padding:  EdgeInsets.symmetric(horizontal: size.width*0.02,vertical: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: size.width * 0.8,
+              height: size.height * 0.05,
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'What do you search for ?',
+                  hintStyle: TextStyle(color: AppColor.primary, fontSize: 16),
+                  focusColor: AppColor.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: BorderSide(color: AppColor.primary, width: 2),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: BorderSide(color: AppColor.primary, width: 2),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(color: Colors.red, width: 2),
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                    borderSide: const BorderSide(color: Colors.red, width: 2),
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search_sharp,
+                    color: AppColor.primary,
+                    size: 30,
+                  ),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(28),
-                  borderSide: BorderSide(color: AppColor.primary, width: 2),
-                ),
-                errorBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(28),
-                  borderSide: const BorderSide(color: Colors.red, width: 2),
-                ),
-                focusedErrorBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(28),
-                  borderSide: const BorderSide(color: Colors.red, width: 2),
-                ),
-                prefixIcon: Icon(
-                  Icons.search_sharp,
-                  color: AppColor.primary,
-                  size: 30,
-                ),
+                cursorColor: AppColor.primary,
               ),
-              cursorColor: AppColor.primary,
             ),
-          ),
-          IconButton(
-            onPressed: () {
-              navigatorKey.currentState!.pushNamed(PageRouteName.shopping);
-            },
-            icon: Icon(
-              Icons.shopping_cart_outlined,
-              size: 30,
-              color: AppColor.primary,
+            IconButton(
+              onPressed: () {
+                navigatorKey.currentState!.pushNamed(PageRouteName.shopping);
+              },
+              icon: Icon(
+                Icons.shopping_cart_outlined,
+                size: 30,
+                color: AppColor.primary,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/features/Presentation/layout/home/home_screen.dart
+++ lib/features/Presentation/layout/home/home_screen.dart
@@ -1,9 +1,35 @@
+import 'dart:async';
 import 'package:again_e_commerce/core/them/app_color.dart';
 import 'package:flutter/material.dart';
 import '../../../../core/model/search_shopping.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<String> imgCategory = [
+    "assets/images/CarouselSlider1.png",
+    "assets/images/CarouselSlider2.png",
+    "assets/images/CarouselSlider3.png",
+  ];
+  int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 2), (timer) {
+      setState(() {
+        currentIndex++;
+        if (currentIndex >= imgCategory.length) {
+          currentIndex = 0;
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +38,68 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         children: [
           SearchShopping(),
+          // BlocBuilder(builder: (context, state) {
+          //   switch (state){
+          //     case LoadingGetCategoriesState():{
+          //       return
+          //     }
+          //   }
+          //   return Column();
+          // },),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(18),
+              child: Image.asset(
+                imgCategory[currentIndex],
+                height: 200,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 6),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Categories",
+                  style: TextStyle(
+                    color: AppColor.primary,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  "View All",
+                  style: TextStyle(
+                    color: AppColor.primary,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    image: DecorationImage(image: AssetImage('assets/images/category_image.jpg'))
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) => SizedBox(width: 10),
+              itemCount: 6,
+            ),
+          ),
         ],
       ),
     );
